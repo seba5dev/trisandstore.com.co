21a977e4852029b8132f3fb525cf8792ab7a3def
Sebastian Martinez <sebasmartinez05@outlook.com>
2022-05-15 21:33:18 -0500

    permitir el sitio web de la api

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    //enter the domain or subdomain where you have WordPress installed
+    domains: ["trisandstore.com.co"],
+  },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
