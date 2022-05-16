e55d79c536644595a86bddc09bd39ba41b04dec2
Sebastian Martinez <sebasmartinez05@outlook.com>
2022-05-15 22:11:01 -0500

    documentar codigo

--- utils/wordpress.js
+++ utils/wordpress.js
@@ -1,23 +1,29 @@
+// Base de la URL de la API
 const BASE_URL = "https://trisandstore.com.co/api/wp-json/wp/v2";
 
+// Función para recuperar los posts de la API
 export async function getPosts() {
   const postsRes = await fetch(BASE_URL + "/posts?_embed");
   const posts = await postsRes.json();
   return posts;
 }
 
+// Función para recuperar un solo post de la API
 export async function getPost(slug) {
   const posts = await getPosts();
   const postArray = posts.filter((post) => post.slug == slug);
   const post = postArray.length > 0 ? postArray[0] : null;
   return post;
 }
+
+// Función para recuperar los productos de la API
 export async function getProductos() {
   const productosRes = await fetch(BASE_URL + "/productos?_embed");
   const productos = await productosRes.json();
   return productos;
 }
 
+// Función para recuperar un solo producto de la API
 export async function getProducto(slug) {
   const productos = await getProductos();
   const productoArray = productos.filter((producto) => producto.slug == slug);
@@ -25,6 +31,7 @@ export async function getProducto(slug) {
   return producto;
 }
 
+// Función para recuperar los slugs de las URL de la API
 export async function getSlugs(type) {
   let elements = [];
   switch (type) {
